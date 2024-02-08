01b3068c269d935409cfa74e26ed7810d6c5f4e5
Khwaish01 <147942846+Khwaish01@users.noreply.github.com>
2024-02-08 22:29:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { useEffect } from "react";
+import {
+  Routes,
+  Route,
+  useNavigationType,
+  useLocation,
+} from "react-router-dom";
+import DESKTOPVIEW from "./pages/DESKTOPVIEW";
+
+function App() {
+  const action = useNavigationType();
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  useEffect(() => {
+    if (action !== "POP") {
+      window.scrollTo(0, 0);
+    }
+  }, [action, pathname]);
+
+  useEffect(() => {
+    let title = "Aspire-Arc";
+    let metaDescription = "Aspire-Arc";
+
+    switch (pathname) {
+      case "/":
+        title = "Aspire-Arc";
+        metaDescription = "Aspire-Arc";
+        break;
+    }
+
+    if (title) {
+      document.title = title;
+    }
+
+    if (metaDescription) {
+      const metaDescriptionTag = document.querySelector(
+        'head > meta[name="description"]'
+      );
+      if (metaDescriptionTag) {
+        metaDescriptionTag.content = metaDescription;
+      }
+    }
+  }, [pathname]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<DESKTOPVIEW />} />
+    </Routes>
+  );
+}
+export default App;
